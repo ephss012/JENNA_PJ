f61eb83840f54bba93c48a97a278272d9a103f09
ephss012 <adesigne@naver.com>
2022-02-07 22:18:34 +0900

    220207

--- 999.portfolio/000.ParksTokyo/js/common.js
+++ 999.portfolio/000.ParksTokyo/js/common.js
@@ -2,4 +2,11 @@ window.addEventListener("DOMContentLoaded",
     () => {
         console.log('로딩완료');
 
+        // (1) 스크롤 위치값
+        let scTop;
+        // (2) 스크롤 등장요소 위치값
+        let scPos = []; // 배열변수
+        // (3) 스크롤 등장요소
+        let scAct = document.querySelectorAll(".scAct");
+
     });
