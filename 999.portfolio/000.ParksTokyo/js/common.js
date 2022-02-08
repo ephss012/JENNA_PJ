745bc516cdadf0161e0646dabd54515732524b03
Tom B. Parker <tombap8@hotmail.com>
2022-02-08 17:57:01 +0900

    ss

--- 999.portfolio/000.ParksTokyo/js/common.js
+++ 999.portfolio/000.ParksTokyo/js/common.js
@@ -2,6 +2,9 @@ window.addEventListener("DOMContentLoaded",
     () => {
         console.log('로딩완료');
 
+        /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+        startSS();
+
         // (1) 스크롤 위치값
         let scTop;
         // (2) 스크롤 등장요소 위치값
@@ -9,4 +12,6 @@ window.addEventListener("DOMContentLoaded",
         // (3) 스크롤 등장요소
         let scAct = document.querySelectorAll(".scAct");
 
-    });
+
+
+    }); /////////////// load /////////////////////////
