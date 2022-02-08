c9aa5f1435c1261be0d2dd32d07c3ac2183ff4a1
Tom B. Parker <tombap8@hotmail.com>
2022-02-08 18:12:43 +0900

    ss

--- 999.portfolio/000.ParksTokyo/js/common.js
+++ 999.portfolio/000.ParksTokyo/js/common.js
@@ -12,6 +12,33 @@ window.addEventListener("DOMContentLoaded",
         // (3) 스크롤 등장요소
         let scAct = document.querySelectorAll(".scAct");
 
+        // (4) 타이틀 박스
+        let titBx = document.querySelector(".cont");
+
+        /////// 스크롤 액션 구역 ///////////////
+        window.addEventListener("scroll",()=>{
+
+            // 스크롤 위치값
+            scTop = this.scrollY;
+            console.log("스위:",scTop);
+
+            // 타이틀 글자 움직이기
+            if(scTop < 50){
+                titBx.style.left = "0";
+            }
+            else if(scTop < 200){
+                titBx.style.left = "-50%";
+            }
+            else if(scTop < 400){
+                titBx.style.left = "-80%";
+            }
+            else if(scTop < 500){
+                titBx.style.left = "-100%";
+            }
+
+        }); ////////////// scroll //////////////////
+        ////////////////////////////////////////////
+
 
 
     }); /////////////// load /////////////////////////
